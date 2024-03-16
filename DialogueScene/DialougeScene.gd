d3b2a89e00c53cc143f84e90663f51272fb36aae
James <ceruleancerise+github@gmail.com>
2024-03-16 15:31:04 -0700

    Day 6.1

--- DialogueScene/DialougeScene.gd
+++ DialogueScene/DialougeScene.gd
@@ -12,13 +12,14 @@ class_name DialogueScene extends Control
 @export var animation_player: AnimationPlayer
 
 @onready var requests: Array[PotionRequest]
+var ingredients: Array[Ingredient]
 
 func _ready() -> void:
 	reset_requests()
 	set_random_request()
 	customer_enter()
 
-func submit_potion(ingredients: Array[Ingredient]):
+func submit_potion(submitted_ingredients: Array[Ingredient]):
 	print("Potion submitted:")
 	var i_1 = "empty"
 	if (ingredients.size() >= 1 && !!ingredients[0]): i_1 = ingredients[0].title
@@ -26,6 +27,10 @@ func submit_potion(ingredients: Array[Ingredient]):
 	if (ingredients.size() >= 2 && !!ingredients[1]): i_2 = ingredients[1].title
 	print(i_1 + ", " + i_2)
 	
+	ingredients = submitted_ingredients
+	animation_player.play("potion_enter")
+		
+func check_potion():
 	var matching_tags = get_matching_ingredient_tags(ingredients)
 	print("Matching tags:")
 	print(matching_tags)
@@ -34,7 +39,13 @@ func submit_potion(ingredients: Array[Ingredient]):
 	print ("Is sufficient:")
 	print(is_sufficient)
 	
-	if (is_sufficient): customer_exit()
+	if (is_sufficient):
+		set_correct_dialogue() 
+		customer_exit()
+	else:
+		set_hint_dialogue(matching_tags)
+		
+	ingredients = []
 
 # Terrible algorithm 
 func get_matching_ingredient_tags(ingredients: Array[Ingredient]) -> Array[String]:
@@ -85,6 +96,29 @@ func customer_enter():
 
 func _on_animation_finished(anim_name: StringName) -> void:
 	match (anim_name):
+		"potion_enter":
+			check_potion()
 		"customer_exit": 
 			set_random_request()
 			customer_enter()
+			
+func set_hint_dialogue(matching_tags: Array[String]):
+	var new_dialogue = ""
+	
+	if (matching_tags.size() == 0):
+		new_dialogue = "That's not quite it. Could you try again?"
+	else:
+		new_dialogue = "Well, the potion is "
+		for i in range(matching_tags.size()):
+			new_dialogue = new_dialogue + matching_tags[i] + ", "
+			if (i == matching_tags.size() - 2): 
+				new_dialogue = new_dialogue + "and "
+		new_dialogue = new_dialogue + " but it's missing something. Mind giving it another try?"
+		
+	new_dialogue = new_dialogue + "\n" + request.dialogue
+	
+	dialogue.set_text(new_dialogue)
+		
+
+func set_correct_dialogue():
+	dialogue.set_text("That's it!\nThanks so much for your help!")
--- IngredientCauldron/IngredientCauldron.gd
+++ IngredientCauldron/IngredientCauldron.gd
@@ -3,18 +3,19 @@ class_name IngredientCauldron extends Control
 var p_IngredientSlot = preload("res://IngredientSlot/IngredientSlot.tscn")
 
 @export var max_ingredient_count: int = 0
-@export var slot: IngredientSlot
+
+@export var slot_1: IngredientSlot
+@export var slot_2: IngredientSlot
 
 var ingredients: Array[Ingredient] = []
 
-var slots: Array[Node]
-
 func clear_slots() -> void:
 	ingredients = []
-	slot.clear_ingredient()
+	slot_1.clear_ingredient()
+	slot_2.clear_ingredient()
 
 func get_ingredients() -> Array[Ingredient]:
-	return ingredients
+	return [slot_1.ingredient, slot_2.ingredient]
 	
 func _on_ingredient_changed(ingredient: Ingredient) -> void:
 	if (ingredients.size() < max_ingredient_count):
